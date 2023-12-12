ea099eb33cde01395c4295b32d07d0a767cc6cff
Jazzyunity (Edenskull) <113677546+Jazzyunity@users.noreply.github.com>
2023-12-12 21:32:33 +0100

    Add Typeorm

---
+++ db/database.js
@@ -0,0 +1,12 @@
+var typeorm = require("typeorm")
+
+var dataSource = new typeorm.DataSource({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "test",
+    password: "admin",
+    database: "test",
+    synchronize: true,
+    entities: [require("./entity/Post"), require("./entity/Category")],
+})
