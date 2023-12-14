04e93df780bfad0e172048845630df466bbf3cd9
Jazzyunity (Edenskull) <113677546+Jazzyunity@users.noreply.github.com>
2023-12-14 14:50:25 +0100

    Add Eventrouter import

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use(cookieParser());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/events', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
